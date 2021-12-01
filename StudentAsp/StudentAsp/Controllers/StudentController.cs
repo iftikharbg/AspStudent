c8028de83d4f0f4112787d3fe8f9fdad04c16c35
iftikharbg <iftikharbg@code.edu.az>
2021-12-01 12:15:55 +0400

    Homework

---
+++ StudentAsp/StudentAsp/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAsp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index(int? id)
+        {
+            if (id == null)
+            {
+                ViewData["message"] = "Id is null";
+                return View("NewsList");
+            }
+
+            return Json(new { Name = "Eli", Surname = "Isaxanli", Age = 21 });
+           
+
+        }
+    }
+}
